5c812e6509457ecc4ef6c2d7b951b266bea76fb4
ravenloue <42080046+ravenloue@users.noreply.github.com>
2025-05-30 22:33:30 -0600

    Swapping from SASS to Tailwind

--- app/src/app.js
+++ app/src/app.js
@@ -2,7 +2,9 @@ myfunction = () => {
     let body = document.body;
     let modeSwitch = document.getElementById("mode");
     body.classList.toggle("darkmode");
-    modeSwitch.classList.toggle("dark");
+    modeSwitch.classList.toggle("translate-x-[-24px]");
+    modeSwitch.classList.toggle("bg-white");
+    modeSwitch.classList.toggle("bg-stone-900");
     console.log("I've been clicked");
 }
 let modeBtn = document.getElementById("modeToggle");
